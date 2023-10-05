ade220c888db783a3db126351cac3f5b0d16e5cb
PsyQox <pikithor@gmail.com>
2023-10-05 11:13:21 -0500

    as

---
+++ client/src/components/PresentationCard/PresentationCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './PresentationCard.module.css'
+
+const PresentationCard = ()=>{
+    return(
+        <div>
+            <h1>presentation card</h1>
+        </div>
+    )
+}
+
+export default PresentationCard
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Home.module.css"
+import PresentationCard from "../../components/PresentationCard/PresentationCard";
+
+const Home = ()=>{
+    return(
+        <div>
+            <h1>Home</h1>
+            <PresentationCard />
+        </div>
+    )
+}
+
+export default Home
