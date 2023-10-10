36c61ee12a83d6ef56da82cb0c7e2e28096fd460
PsyQox <pikithor@gmail.com>
2023-10-10 17:31:51 -0500

    as

--- src/components/ProyectsCards/ProyectCard/ProyectCard.jsx
+++ src/components/ProyectsCards/ProyectCard/ProyectCard.jsx
@@ -1,33 +1,45 @@
 import React from "react";
 import style from "./ProyectCard.module.css"
+import Food from "../../../assets/img/food.png"
+import {Link} from "react-router-dom"
 
 const ProyectCard = ()=>{
 
     return(
         <div className={style.containerP}>
             <div className={style.containerCard}>
-                    <img className={style.image} src="https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg" alt=""/>
-                <h2>nombre</h2>
-                <div>
-                    <button>Github</button>
-                    <button>deploy</button>
-                </div>
+                    <img className={style.image} src={Food} alt=""/>
+                    <div className={style.overlay}>
+                        <h2>Food</h2>
+                        <div>
+                            <Link to="https://github.com/PsyQox/food" target="_blank" rel="noopener noreferrer">
+                                <button className={style.button}>GitHub</button>
+                            </Link>
+                            <Link to="https://food-front-flax.vercel.app" target="_blank" rel="noopener noreferrer">
+                                <button>Deploy</button>
+                            </Link>
+                        </div>
+                    </div>
             </div>
             <div className={style.containerCard}>
                     <img className={style.image} src="https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg" alt=""/>
-                <h2>nombre</h2>
-                <div>
-                    <button>Github</button>
-                    <button>deploy</button>
-                </div>
+                    <div className={style.overlay}>
+                        <h2>nombre</h2>
+                        <div>
+                            <button>Github</button>
+                            <button>deploy</button>
+                        </div>
+                    </div>
             </div>
             <div className={style.containerCard}>
                     <img className={style.image} src="https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg" alt=""/>
-                <h2>nombre</h2>
-                <div>
-                    <button>Github</button>
-                    <button>deploy</button>
-                </div>
+                    <div className={style.overlay}>
+                        <h2>nombre</h2>
+                        <div>
+                            <button>Github</button>
+                            <button>deploy</button>
+                        </div>
+                    </div>
             </div>
         </div>
     )
